b299588175073d36373fbbaabd76243832c0ce22
Gustavo Silva <gus7silva@berkeley.edu>
2016-02-29 14:13:42 -0800

    Watch 2D picker somewhat functional

---
+++ App/wear/src/main/java/gusrsilva/represent/CardAdapter.java
@@ -0,0 +1,74 @@
+package gusrsilva.represent;
+
+import android.content.Context;
+import android.support.v4.content.ContextCompat;
+import android.support.wearable.view.BoxInsetLayout;
+import android.support.wearable.view.GridPagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.zip.Inflater;
+
+/**
+ * Created by GusSilva on 2/29/16.
+ */
+public class CardAdapter extends GridPagerAdapter {
+
+    ArrayList<Rep> repList;
+    Context mContext;
+
+    public CardAdapter(ArrayList<Rep> reps, Context context)
+    {
+        repList = reps;
+        mContext = context;
+    }
+
+    @Override
+    public int getRowCount() {
+        //TODO: Dynamically calculate
+        return 1;
+    }
+
+    @Override
+    public int getColumnCount(int i) {
+        return repList.size();
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup viewGroup, int row, int col) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.rep_card, null);
+
+        Rep rep = repList.get(col);
+        TextView type, name, party;
+        BoxInsetLayout holder = (BoxInsetLayout)view.findViewById(R.id.holder);
+        type = (TextView)view.findViewById(R.id.type);
+        name = (TextView)view.findViewById(R.id.name);
+        party = (TextView)view.findViewById(R.id.party);
+
+        type.setText(rep.getRepType());
+        name.setText(rep.getName());
+        party.setText(rep.getParty());
+        party.setTextColor(ContextCompat.getColor(mContext, R.color.dem_blue)); //TODO: Dynamically get color
+        holder.setBackgroundResource(rep.getImageResource());
+
+
+        viewGroup.addView(view);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup viewGroup, int i, int i1, Object o) {
+        viewGroup.removeView((View) o);
+
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object o) {
+        return view.equals(o);
+    }
+}
+
--- App/wear/src/main/java/gusrsilva/represent/MainActivity.java
+++ App/wear/src/main/java/gusrsilva/represent/MainActivity.java
@@ -2,8 +2,11 @@ package gusrsilva.represent;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.wearable.view.GridViewPager;
 import android.support.wearable.view.WatchViewStub;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends Activity {
 
@@ -13,12 +16,20 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
-        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
-            @Override
-            public void onLayoutInflated(WatchViewStub stub) {
-                mTextView = (TextView) stub.findViewById(R.id.text);
-            }
-        });
+        final GridViewPager pager = (GridViewPager)findViewById(R.id.pager);
+        //PagerAdapter adapter = new PagerAdapter(getApplicationContext(), getFragmentManager());
+
+        ArrayList<Rep> repList = new ArrayList<>();
+        Rep boxer = new Rep();
+        boxer.setRepType("Senator");
+        boxer.setName("Barbara Boxer");
+        boxer.setParty("Democrat");
+        boxer.setEmail("Sen.Boxer@opencongress.org");
+        boxer.setWebsite("www.boxer.senate.gov");
+        boxer.setImageResource(R.drawable.rep1);
+
+        repList.add(boxer);repList.add(boxer);repList.add(boxer);repList.add(boxer);
+        CardAdapter adapter = new CardAdapter(repList, getApplicationContext());
+        pager.setAdapter(adapter);
     }
 }
